0631a1c80c8a6185aeb7cba1bee1311eb4e83a06
christianturner <christiandavidphoto@gmail.com>
2021-02-10 15:26:54 -0500

    resolved local port & added two pages

--- main.go
+++ main.go
@@ -2,10 +2,26 @@ package main
 
 
 import (
-	"fmt",
+	"fmt"
 	"net/http"
 )
 
+// w = writer & writer puts info on page
+// ResponseWriter is a custom type
+func indexHandler(w http.ResponseWriter, r *http.Request) { // & = memory address, *is the request
+	// using our writer, it will write the string
+	fmt.Fprintf(w, "Ran! That's pretty cool.")
+}
+
+func aboutHandler(w http.ResponseWriter, r *http.Request) { // & = memory address, *is the request
+	// using our writer, it will write the string
+	fmt.Fprintf(w, "A second page! What a thought!")
+}
+
 func main(){
-	
+	http.HandleFunc("/", indexHandler) // takes in path (homepage) "/", and our handler
+	http.HandleFunc("/about", aboutHandler) // takes in path (homepage) "/", and our handler
+	http.ListenAndServe(":8000", nil) 	// first arg is port, second is nil for now (for server)
+
+
 }
