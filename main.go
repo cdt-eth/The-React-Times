1ce6eaebf6e2996faf06c9c04f24e77f0dd3e5c9
christianturner <christiandavidphoto@gmail.com>
2021-02-11 13:43:24 -0500

    perf increse with goroutine

--- main.go
+++ main.go
@@ -7,7 +7,10 @@ import (
 	"io/ioutil"     // input output
 	"net/http"      // HTTP Requests (GET, POST, etc)
 	"strings"       // string package
+	"sync"          // concurrency
 )
+
+var wg sync.WaitGroup
 
 // SitemapIndex ...
 // greater than means it get the next location down
@@ -36,22 +39,37 @@ type NewsAggPage struct {
 	News  map[string]NewsMap
 }
 
-func newsAggHandler(w http.ResponseWriter, r *http.Request) {
-	var s SitemapIndex //assign variable
+func newsRoutine(c chan News, Location string) {
+	defer wg.Done()
 	var n News
-	newsMap := make(map[string]NewsMap)
+	Location = strings.TrimSpace(Location) // trim whitespace
+	resp, _ := http.Get(Location)          // response
+	bytes, _ := ioutil.ReadAll(resp.Body)  // GET request
+	xml.Unmarshal(bytes, &n)               // unmarshal into news struct
+	resp.Body.Close()                      // close to prevent resource leak
+	c <- n                                 // put the  values on the channel inside the goroutine
+}
+
+func newsAggHandler(w http.ResponseWriter, r *http.Request) {
+	var s SitemapIndex                                                            //assign variable
 	resp, _ := http.Get("https://www.washingtonpost.com/news-sitemaps/index.xml") // response
 	bytes, _ := ioutil.ReadAll(resp.Body)                                         // GET request
 	xml.Unmarshal(bytes, &s)                                                      // parse data
+	newsMap := make(map[string]NewsMap)
+	resp.Body.Close() // close to prevent resource leak
+	queue := make(chan News, 30)
 
 	for _, Location := range s.Locations {
-		Location = strings.TrimSpace(Location) // trim whitespace
-		resp, _ := http.Get(Location)          // response
-		bytes, _ := ioutil.ReadAll(resp.Body)  // GET request
-		xml.Unmarshal(bytes, &n)               // unmarshal into news struct
+		wg.Add(1)                       // as we iterate we need add 1 to wg to make room
+		go newsRoutine(queue, Location) // pass `queue` as the channel tothe goroutine
+	}
 
-		for index := range n.Keywords {
-			newsMap[n.Titles[index]] = NewsMap{n.Keywords[index], n.Locations[index]}
+	wg.Wait()
+	close(queue) // close the queue
+
+	for e := range queue { // iterate over channel, e is the News type - i.e. "element"
+		for index := range e.Keywords {
+			newsMap[e.Titles[index]] = NewsMap{e.Keywords[index], e.Locations[index]}
 		}
 	}
 
