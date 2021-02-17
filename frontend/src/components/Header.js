32f2c755534d9d4bc409422d2b6a9bfe4e72d703
christianturner <christiandavidphoto@gmail.com>
2021-02-16 23:09:50 -0500

    add credit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -7,6 +7,21 @@ export default class Header extends React.Component {
       <header className="App-header">
         <h1>{this.props.title}</h1>
         <img src={logo} alt="golang-gopher" />
+
+        <p className="credit">
+          work in progress:{" "}
+          <a href="https://twitter.com/imcdt" target="_blank" rel="noreferrer">
+            me
+          </a>{" "}
+          |{" "}
+          <a
+            href="https://github.com/christiandavidturner/Go-News-Aggregator"
+            target="_blank"
+            rel="noreferrer"
+          >
+            repo
+          </a>
+        </p>
       </header>
     );
   }
