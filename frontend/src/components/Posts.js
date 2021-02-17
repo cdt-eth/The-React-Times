50f354439ddca1a22c800b3736f9cadb208de6c1
christianturner <christiandavidphoto@gmail.com>
2021-02-17 16:24:09 -0500

    cleanup

--- frontend/src/components/Posts.js
+++ frontend/src/components/Posts.js
@@ -17,9 +17,6 @@ const Posts = (props) => {
           setIsLoaded(true);
           setItems(result);
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
         (error) => {
           setIsLoaded(true);
           setError(error);
