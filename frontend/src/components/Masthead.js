7e019c79b623a350101fb693b8e2b230a44c544d
christianturner <christiandavidphoto@gmail.com>
2021-02-23 23:56:17 -0500

    add active class to selected nav item

--- frontend/src/components/Masthead.js
+++ frontend/src/components/Masthead.js
@@ -14,11 +14,18 @@ var sections = [
 ];
 
 export default function Masthead() {
+  const [cur_section, setCurSection] = React.useState("");
+
   return (
     <div className="masthead">
       {sections.map((section, idx) => {
         return (
-          <a href={section} key={idx}>
+          <a
+            href={"#" + section}
+            key={idx}
+            onClick={() => setCurSection(section)}
+            className={section === cur_section ? "active" : ""}
+          >
             <span>{section}</span>
           </a>
         );
