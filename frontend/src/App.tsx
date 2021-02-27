8bd28dabc585edc3d8e4dbe6a5b47a83157bb124
christianturner <christiandavidphoto@gmail.com>
2021-02-26 22:49:47 -0500

    install typescript, setup ts-ignore flags, set jsx compmiler to react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,11 @@
 import React from "react";
+// @ts-ignore
 import Header from "./components/Header";
+// @ts-ignore
 import News from "./components/News";
+// @ts-ignore
 import Footer from "./components/Footer";
+// @ts-ignore
 import Spacer from "./components/Spacer";
 import "./App.css";
 
