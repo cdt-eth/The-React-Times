ed335260409ad23efa0090b5a0ef95bdfb0450a6
christianturner <christiandavidphoto@gmail.com>
2021-03-03 10:10:30 -0500

    first unit tests added

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,34 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
+import Header from "./components/Header";
 
-test('renders learn react link', () => {
+test("render App Component", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+});
+
+describe("Logo", () => {
+  test('Logo must have src = "https://media1.giphy.com/media/eNAsjO55tPbgaor7ma/giphy.gif" and alt = "react-logo-gif"', () => {
+    render(<Header />);
+    const logo = screen.getByRole("img");
+    expect(logo).toHaveAttribute(
+      "src",
+      "https://media1.giphy.com/media/eNAsjO55tPbgaor7ma/giphy.gif"
+    );
+    expect(logo).toHaveAttribute("alt", "react-logo-gif");
+  });
+});
+
+test("check header links", () => {
+  const { getByText } = render(<Header />);
+  const a = getByText("me");
+  expect(a).toBeTruthy();
+
+  expect(screen.getByText("me").closest("a")).toHaveAttribute(
+    "href",
+    "https://cdt.works/"
+  );
+  expect(screen.getByText("repo").closest("a")).toHaveAttribute(
+    "href",
+    "https://github.com/christiandavidturner/The-React-Times"
+  );
 });
